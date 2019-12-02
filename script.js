f1be6dd19d43a0d75458d223c0abb65d81f8ff26
Seo  cheongwon <woon4754@gmail.com>
2019-12-02 12:29:05 +0000

    js lecture 1-10 Object, array

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ a = 4;
 // const = > 변하지 않는 변수 앞에
 // let, var => 변해도 되는 변수 앞에
 console.log(b, a);
+
+//Object, Array 
+const coneseo = {
+  name : "coneseo",
+  age : 29,
+  isHandsome : true,
+  favMovies : ["the king of the rings", "big bang theory"],
+  favFood : [{name : "burger", isFatty : true}, {name : "gummies",
+  isFatty : true}]
+}
+
+console.log(coneseo.favFood);
