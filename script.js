f26c07b27775fff578ee71fbe9f2681f922a0e04
Seo  cheongwon <woon4754@gmail.com>
2019-12-04 04:04:16 +0000

    js lecture 2-1 sayHello(name, age)

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-var a = 221;
-var b = a - 6;
-a = 4;
+//var a = 221;
+//var b = a - 6;
+//a = 4;
 // const = > 변하지 않는 변수 앞에
 // let, var => 변해도 되는 변수 앞에
-console.log(b, a);
+//console.log(b, a);
 
 //Object, Array 
 const coneseo = {
@@ -14,5 +14,14 @@ const coneseo = {
   favFood : [{name : "burger", isFatty : true}, {name : "gummies",
   isFatty : true}]
 }
+let a = ["a","b"];
+const c = [a, "1", "2"]
+console.log(c);
+const array = ["a", "b", {key : "sdf"}]
+console.log(array[2].key);
 
-console.log(coneseo.favFood);
+function sayHello(name, age){
+  console.log("hello ",name," you have ",age)
+}
+
+sayHello("coneseo", 29);
