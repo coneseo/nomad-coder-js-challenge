9f7519cafc2a088bd99f8c4c4ca3d97a6dfda95b
coneseo <woon4754@gmail.com>
2019-12-04 22:03:37 +0900

    js lecture 2.4 window.addEventListener(이벤트, function);

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const title = document.querySelector('#title');
 
-title.innerHTML = "Document Object Memory!";
-title.style.color = "red";
+function resizeMessage(){
+  console.log("resizing message!");
+}
+
+window.addEventListener("resize", resizeMessage)
