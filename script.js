027738b2db2dcc2b474b3b60a23eae9ef6d2bc74
coneseo <woon4754@gmail.com>
2019-12-05 01:09:30 +0900

    js challenge day3, addEventListener(event, function)

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
-const title = document.querySelector('#title');
 
-function resizeMessage(event){
-  console.log(event);
+const title = document.querySelector("#title");
+const colors = ["#1abc9c", "#3498db", "#9b59b6", "#f39c12", "#e74c3c"];
+function clickEvent(){
+  let color = title.style.color;
+  if(title.style.color == "blue"){
+    title.style.color = "red";
+  }else{
+    title.style.color = "blue";
+  }
 }
+title.addEventListener("click", clickEvent)
 
-window.addEventListener("resize", resizeMessage)
+const superEventHandler = {
+  mouseOnTop: function() {
+    title.innerHTML = "The mouse is here!";
+    title.style.color = colors[0];
+  },
+  mouseLeave: function() {
+    title.innerHTML = "The mouse is gone!";
+    title.style.color = colors[1];
+  },
+  resize: function() {
+    title.innerHTML = "You just Resized!";
+    title.style.color = colors[2];
+  },
+  rightClick: function() {
+    title.innerHTML = "this is right click!";
+    title.style.color = colors[3];
+  }
+};
+
+title.addEventListener("mouseover", superEventHandler.mouseOnTop);
+title.addEventListener("mouseleave", superEventHandler.mouseLeave);
+window.addEventListener("resize", superEventHandler.resize);
+window.addEventListener("contextmenu", superEventHandler.rightClick);
