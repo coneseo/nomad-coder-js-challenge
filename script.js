0133ebc8e6f2b6272fa07d06550ba206473ef88f
coneseo <woon4754@gmail.com>
2019-12-06 01:11:34 +0900

    js lecture 2.7 toggle의 사용 mdn을 제대로 읽자

--- script.js
+++ script.js
@@ -2,12 +2,14 @@
 const title = document.querySelector("#title");
 const className = "title";
 function changeColor(){
-  const hasClass = title.classList.contains(className);
-  if(hasClass){
-    title.classList.remove(className);
-  }else{
-    title.classList.add(className);
-  }
+  // const hasClass = title.classList.contains(className);
+  // if(hasClass){
+  //   title.classList.remove(className);
+  // }else{
+  //   title.classList.add(className);
+  // }
+  //toggle 이라는 기능의 사용은 위 5줄의 코드를 1줄로 줄여준다.
+  title.classList.toggle(className);
 }
 
 window.addEventListener("click", changeColor);
