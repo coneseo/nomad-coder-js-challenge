f479b882f9eacccefe93e24ceb2e36cb438a13a5
coneseo <woon4754@gmail.com>
2019-12-06 01:06:10 +0900

    js lecture 2.7 각자 역할에 맞는 사용법의 분할!(css, js) - class 로 나눠서 처ㅣ

--- script.js
+++ script.js
@@ -1,47 +1,16 @@
 
 const title = document.querySelector("#title");
-const colors = ["#1abc9c", "#3498db", "#9b59b6", "#f39c12", "#e74c3c"];
-function clickEvent(){
-  let color = title.style.color;
-  if(title.style.color == "blue"){
-    title.style.color = "red";
+const className = "title";
+function changeColor(){
+  const hasClass = title.classList.contains(className);
+  if(!hasClass){
+    title.classList.add(className);
   }else{
-    title.style.color = "blue";
+    title.classList.remove(className);
   }
 }
-title.addEventListener("click", clickEvent)
 
-const superEventHandler = {
-  mouseOnTop: function() {
-    title.innerHTML = "The mouse is here!";
-    title.style.color = colors[0];
-  },
-  mouseLeave: function() {
-    title.innerHTML = "The mouse is gone!";
-    title.style.color = colors[1];
-  },
-  resize: function() {
-    title.innerHTML = "You just Resized!";
-    title.style.color = colors[2];
-  },
-  rightClick: function() {
-    title.innerHTML = "this is right click!";
-    title.style.color = colors[3];
-  }
-};
+window.addEventListener("click", changeColor);
 
-title.addEventListener("mouseover", superEventHandler.mouseOnTop);
-title.addEventListener("mouseleave", superEventHandler.mouseLeave);
-window.addEventListener("resize", superEventHandler.resize);
-window.addEventListener("contextmenu", superEventHandler.rightClick);
 
-//wifi 상태를 감지하는 eventlistenr
-function handleOffline(){
-  console.log("bye bye");
-}
 
-function handleOnline(){
-  console.log("hello");
-}
-window.addEventListener("offline", handleOffline);
-window.addEventListener("online", handleOnline);
