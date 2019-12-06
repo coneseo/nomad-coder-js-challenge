382b80cfb45db026f1f02221f25c5f366a778c30
coneseo <woon4754@gmail.com>
2019-12-07 04:04:05 +0900

    js challenge day4 Time Until xmas

--- script.js
+++ script.js
@@ -32,17 +32,26 @@ window.addEventListener("resize", changeColor);
 
 const clock = document.querySelector(".clock");
 function getTime(){
-  const date = new Date();
-  const hours = date.getHours();
-  const minutes = date.getMinutes();
-  const seconds = date.getSeconds();
-  clock.innerHTML = `${hours < 10 ? `0${hours}` : hours}
-  :${minutes < 10 ? `0${minutes}` : minutes}:
-  ${seconds < 10 ? `0${seconds}`: seconds}`;
+  const now = new Date();
+  // Don't delete this.
+  const xmasDay = new Date("2019-12-24:00:00:00+0900");
+  const remainDays = xmasDay - now ;
+  const days = Math.floor(remainDays / (24 * 60 * 60 * 1000));
+  const hours = Math.floor(
+    (remainDays % (24 * 60 * 60 * 1000)) / (60 * 60 * 1000)
+  );
+  const minutes = Math.floor((remainDays % (60 * 60 * 1000)) / (60 * 1000));
+  const seconds = Math.floor((remainDays % (60 * 1000)) / 1000);
 
+  clock.innerHTML = `${days < 10 ? `0${days}` : days}d ${hours < 10 ? `0${hours}`:hours}h 
+  ${minutes < 10 ? `0${minutes}`:minutes}m ${seconds < 10 ? `0${seconds}` : seconds}s`;
 }
 
-setInterval(getTime, 1000);
+function init(){
+  setInterval(getTime, 1000);
+}
+
+init();
 
 
 
