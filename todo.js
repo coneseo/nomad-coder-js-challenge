da47d7869fa16ad7a3143455f37399c0a765e699
coneseo <woon4754@gmail.com>
2019-12-09 23:58:08 +0900

    js lecture 3.5 todo list(createElement, append, innerText)

--- todo.js
+++ todo.js
@@ -6,15 +6,25 @@ const TODOS_LS = "toDos";
 
 function paintValue(text){
     console.log(text);
+    const li = document.createElement("li");
+    const delBtn = document.createElement("button");
+    const span = document.createElement("span");
+    span.innerText = text;
+    delBtn.innerText = "❌"
+    li.append(delBtn);
+    li.append(span);
+    todoList.append(li);
+    
 }
 function handleSubmit(event){
     event.preventDefault();
     const value = toDoInput.value;
     paintValue(value);
+    toDoInput.value = "";
 }
 
 function loadToDos(){
-
+    const toDos = localStorage.getItem(TODOS_LS);
 }
 function init(){
     loadToDos();
